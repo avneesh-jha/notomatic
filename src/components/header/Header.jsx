b6f6195f09b8548bbab5c75a65e469b7dc7cf232
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-14 09:21:40 +0530

    feat:created a logo and button for header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import { Logo } from "../logo/index";
+import logoSrc from "../../assets/images/logo.png";
+import { ButtonPrimary } from "../buttonPrimary/ButtonPrimary";
+import s from "./style.module.css";
+import { useNavigate } from "react-router-dom";
+export function Header() {
+  const navigate = useNavigate();
+  return (
+    <div className={`row ${s.conatiner}`}>
+      <div className="col-xs-12 col-sm-4">
+        <Logo
+          onClick={() => {
+            navigate("/");
+          }}
+          title={"Notomatic"}
+          subtitle={"Manage your notes"}
+          image={logoSrc}
+        />
+      </div>
+      <div className="col-xs-12 col-sm-8 text-end">
+        <ButtonPrimary
+          onClick={() => {
+            navigate("/note/new");
+          }}
+        >
+          Add Note +
+        </ButtonPrimary>
+      </div>
+    </div>
+  );
+}
