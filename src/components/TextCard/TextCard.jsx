7e08c983e3d4ba61d3b195bf75022ab3d3b69fda
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-16 07:50:24 +0530

    feat: onClick events on both card and trash option and stop propagation option

--- src/components/TextCard/TextCard.jsx
+++ src/components/TextCard/TextCard.jsx
@@ -4,18 +4,24 @@ import { Trash } from "react-bootstrap-icons";
 export function TextCard({ title, subtitle, content, onClick, onClickTrash }) {
   const [isCardHovered, setIsCardHovered] = useState(false);
   const [isTrashHovered, setIsTrashHovered] = useState(false);
+  function onClickTrash_(e) {
+    onClickTrash();
+    e.stopPropagation();
+  }
 
   return (
     <div
-      class="card"
+      onClick={onClick}
+      className="card"
       style={{ borderColor: isCardHovered ? "blue" : "transparent" }}
       onMouseEnter={() => setIsCardHovered(true)}
       onMouseLeave={() => setIsCardHovered(false)}
     >
-      <div class="card-body">
+      <div className="card-body">
         <div>
-          <h5 class="card-title">{title}</h5>
+          <h5 className="card-title">{title}</h5>
           <Trash
+            onClick={onClickTrash_}
             size={20}
             style={{ color: isTrashHovered ? "red" : "blue" }}
             onMouseEnter={() => setIsTrashHovered(true)}
@@ -23,8 +29,8 @@ export function TextCard({ title, subtitle, content, onClick, onClickTrash }) {
           />
         </div>
 
-        <h6 class="card-subtitle mb-2 text-body-secondary">{subtitle}</h6>
-        <p class="card-text">{content}</p>
+        <h6 className="card-subtitle mb-2 text-body-secondary">{subtitle}</h6>
+        <p className="card-text">{content}</p>
       </div>
     </div>
   );
