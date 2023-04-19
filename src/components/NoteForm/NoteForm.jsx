dd1a05630edf01ad81587eed3bff958b2310de21
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-19 22:42:42 +0530

    feat:post req to send notes and create a note in db

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -2,28 +2,41 @@ import { PencilFill, TrashFill } from "react-bootstrap-icons";
 // import { Button } from "react-bootstrap/Button";
 import { ButtonPrimary } from "../../components/ButtonPrimary/ButtonPrimary";
 import s from "./style.module.css";
+import { useState } from "react";
 
-export function NoteForm({ title }) {
+export function NoteForm({ title, onCLickEdit, onClickDelete, onSubmit }) {
+  const [formValues, setFormValues] = useState({ title: "", content: "" });
+  function onChangeFormValues(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormValues({ ...formValues, [name]: value });
+  }
   const actionIcons = (
     <>
       <div className="col-1">
-        <PencilFill className={s.icons} />
+        {onCLickEdit && <PencilFill className={s.icons} />}
       </div>
       <div className="col-1">
-        <TrashFill className={s.icons} />
+        {onClickDelete && <TrashFill className={s.icons} />}
       </div>
     </>
   );
   const titleInput = (
     <>
       <label className="form-lable">Title</label>
-      <input type="text" name="title" className="form-control"></input>
+      <input
+        onChange={onChangeFormValues}
+        type="text"
+        name="title"
+        className="form-control"
+      ></input>
     </>
   );
   const contentInput = (
     <>
       <label className="form-lable">Content</label>
       <textarea
+        onChange={onChangeFormValues}
         type="text"
         name="content"
         className="form-control"
@@ -33,7 +46,7 @@ export function NoteForm({ title }) {
   );
   const submitBtn = (
     <div className={s.submit_btn}>
-      <ButtonPrimary>submit</ButtonPrimary>
+      <ButtonPrimary onClick={() => onSubmit(formValues)}>submit</ButtonPrimary>
     </div>
   );
   return (
@@ -46,7 +59,7 @@ export function NoteForm({ title }) {
       </div>
       <div className={`mb-3${s.title_input_container}`}>{titleInput}</div>
       <div className="mb-3">{contentInput}</div>
-      {submitBtn}
+      {onSubmit && submitBtn}
     </div>
   );
 }
--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,9 +1,19 @@
 import { NoteForm } from "../../components/NoteForm/NoteForm";
-
+import { NoteAPI } from "../../api/note-api";
+import { useDispatch } from "react-redux";
+import { addNoteList } from "../../store/notes/notes-slice";
 export function NoteCreate() {
+  const dispatch = useDispatch();
+  const onClickSubmit = async (formValues) => {
+    const createdNote = await NoteAPI.create({
+      ...formValues,
+      created_at: new Date().toLocaleDateString(),
+    });
+    dispatch(addNoteList(createdNote));
+  };
   return (
     <div>
-      <NoteForm title="New Note" />
+      <NoteForm title="New Note" onSubmit={onClickSubmit} />
     </div>
   );
 }
