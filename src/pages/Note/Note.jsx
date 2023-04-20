347d0b1333224aff46e994898d0f9653027e77bc
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-21 01:51:30 +0530

    buttons and icons done for edit note

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,21 +1,30 @@
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { NoteForm } from "../../components/NoteForm/NoteForm";
+import { useState } from "react";
 
 export function Note() {
   const { noteId } = useParams();
   const note = useSelector((store) =>
     store.noteSlice.noteList.find((note) => note.id === noteId)
   );
+  const [isEditable, setIsEditable] = useState();
+  function editIt() {
+    setIsEditable(!isEditable);
+  }
+  const submit = () => {
+    alert("yes");
+  };
   return (
     <div>
       {note && (
         <NoteForm
-          isEditable={false}
-          title={note.title}
+          isEditable={isEditable}
+          title={isEditable ? "Edit the note" : note.title}
           note={note}
-          onCLickEdit={() => alert("edited")}
+          onCLickEdit={editIt}
           onClickDelete={() => alert("deleted")}
+          onSubmit={isEditable && submit}
         />
       )}
     </div>
