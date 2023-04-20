b2f34fac685fe49d12c173a722b2475ce5c98277
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-21 04:13:38 +0530

    feta:added search bar with css

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,12 +1,14 @@
 import { NoteList } from "../../containers/NoteList";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
 
 export function NoteBrowse() {
   const noteList = useSelector((store) => store.noteSlice.noteList);
 
   return (
     <div>
+      <SearchBar onTextChange={() => console.log("yes")} placeholder="" />
       {noteList?.length === 0 && (
         <div className="d-flex justify">
           <span>
