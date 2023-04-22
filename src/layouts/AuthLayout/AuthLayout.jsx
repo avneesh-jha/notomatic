7bc93a4a9a6cc4d219505d49630c45fd4e88653e
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2023-04-22 07:14:12 +0530

    layout section set for for auth comp

---
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import s from "./style.module.css";
+import { ReactComponent as LogoSVG } from "../../assets/images/logo.svg";
+export function AuthLayout({ children }) {
+  const header = (
+    <div className={s.header}>
+      <LogoSVG className={s.logoTop} />
+      <h3 className={s.logoTitle}>NotoMatic</h3>
+    </div>
+  );
+  const background = (
+    <div className={s.background}>
+      <div className="d-flex">
+        <LogoSVG className={s.logoRight} />
+        <h1 className={s.backgroundText}>NotoMatic</h1>
+      </div>
+      <p style={{ color: "white" }}>One Place for all your Notes</p>
+    </div>
+  );
+  return (
+    <div className={s.root}>
+      <div className={s.leftSection}>
+        {header}
+        {children}
+      </div>
+      <div className={s.rightSection}>{background}</div>
+    </div>
+  );
+}
